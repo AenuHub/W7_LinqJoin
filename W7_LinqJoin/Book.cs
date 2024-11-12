eb362734c9a218a33cd8bb616f371040519e9145
AenuHub <jesuskane92@gmail.com>
2024-11-12 11:35:57 +0300

    create Book.cs

---
+++ W7_LinqJoin/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace W7_LinqJoin;
+
+public class Book
+{
+    public int BookID { get; set; }
+    public string Title { get; set; }
+    public int AuthorID { get; set; }
+    
+    public Book(int bookID, string title, int authorID)
+    {
+        BookID = bookID;
+        Title = title;
+        AuthorID = authorID;
+    }
+}
