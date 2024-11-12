eeee5375bf8b432704a5aba8df604d02183c9df3
AenuHub <jesuskane92@gmail.com>
2024-11-12 11:34:46 +0300

    create Author.cs

---
+++ W7_LinqJoin/Author.cs
@@ -0,0 +1,13 @@
+﻿namespace W7_LinqJoin;
+
+public class Author
+{
+    public int AuthorID { get; set; }
+    public string Name { get; set; }
+
+    public Author(int authorId, string name)
+    {
+        AuthorID = authorId;
+        Name = name;
+    }
+}
